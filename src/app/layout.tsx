376789c89a3385519bb5d9cc84fd39772e999298
faiinrd <nopradakrit@gmail.com>
2024-10-05 18:35:48 +0700

    add name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,8 +80,8 @@ export default function RootLayout({
               </Title>
               {children}
               <Footer
-                studentId="660610999"
-                fullName="Dome Potikanond"
+                studentId="660610766"
+                fullName="Noprada Kritwattananont"
                 year="2024"
               />
             </Container>
